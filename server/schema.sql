51d84fd881e1f754382a45e9077d3adf0e3f4a48
samMahani <sam.amirmahani@gmail.com>
2018-07-07 11:15:08 -0700

    Adds users and room tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,19 +1,34 @@
-DROP DATABASE chat; 
+DROP DATABASE if exists chat; 
 
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE rooms (
   id INTEGER AUTO_INCREMENT NOT NULL,
-  username VARCHAR(20),
-  text text,
   roomname VARCHAR(20),
-  PRIMARY KEY (id)  
+  PRIMARY KEY (id)
 );
 
-INSERT INTO messages (username, text, roomname)
-VALUES ('Diane', 'Where is puffball?!', 'main'), ('Sam', 'Taking so many classes', 'main');
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  username VARCHAR(20),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id INTEGER AUTO_INCREMENT NOT NULL,
+  user_id INTEGER NOT NULL,
+  text text,
+  room_id INTEGER NOT NULL,
+  createdAt timestamp DEFAULT now() ON UPDATE now(),
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)  
+);
+
+-- INSERT INTO messages (username, text, roomname)
+-- VALUES ('Diane', 'Where is puffball?!', 'main'), ('Sam', 'Taking so many classes', 'main'), ('Steve', 'Oh', 'main');
 
 
 
