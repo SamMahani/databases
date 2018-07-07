e9aa52347241f1430c9a588547c617e35944ca28
samMahani <sam.amirmahani@gmail.com>
2018-07-06 19:44:33 -0700

    Add Client from HR branch and Create Table messages

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,46 @@ CREATE DATABASE chat;
 
 USE chat;
 
+  -- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS messages;
+    
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NOT NULL,
+  `username` VARCHAR(20) NULL DEFAULT NULL,
+  `text` VARCHAR(200) NULL DEFAULT NULL,
+  `roomname` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`username`,`text`,`roomname`) VALUES
+-- ('','','','');
+
 
 /* Create other tables and define schemas for them here! */
 
